fd2f78505578f2ad42270b4861f0adf7021e00cf
Sriram <sriram9159541445@gmail.com>
2020-07-12 13:40:10 +0530

    Express REST API with MongoDB and Mongoose Part 2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 connect.then((db) => {
   console.log('Connected correctly to the server');
